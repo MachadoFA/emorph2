c6eb78a5a24a42b5266e35163be47d6db58c800c
FabioLugar <macfabio@gmail.com>
2019-11-08 17:38:20 -0500

    changes in function npEigentest

--- R/npEigentest.R
+++ R/npEigentest.R
@@ -14,18 +14,17 @@
 #'  npEigentest returns a list containing summary of test results, simmulated
 #'  values for each test, and observed results for empirical data.
 #'  \itemize{
-#'      \item{TestResults}{ summary table containing observed values and test results. TRUE = rejected drift.}
-#'      \item{Empirical}{ Test results for empirical data.}
-#'      \item{SimValues}{ simulated values for each test.}
+#'      \item{Empirical}{ Empirical statistic values.}
+#'      \item{SimValues}{ Simulated statistic values.}
+#'      \item{eigenvalues}{ Relationship between empirical within-species
+#'      eigenvalues and between species variances.}
 #'      }
 #'
 #' @examples
 #' data("Canidae")
-#' test.out<-npEigentest(G=W, means, tree, n.s, sims = 100, dim.ret = 20)
+#' test.out<-npEigentest(G = W, means, tree, n.s, sims = 100, dim.ret = 20)
 #' test.out$SimValues
 #' @export
-
-
 
 npEigentest<-function(G,means,phy,n.s,sims=1000,dim.ret=NULL,parallel=FALSE){
   #Estimate the rate matrix from Independant contrasts. V/CV matrix of
@@ -36,6 +35,12 @@ npEigentest<-function(G,means,phy,n.s,sims=1000,dim.ret=NULL,parallel=FALSE){
 
   # Empirical test
   obs<-pcTests(G,R,length(n.s))
+  # Obtaining empirical distribution of eigenvalues
+  eigenG<-eigen(G)
+  if(is.null(dim.ret)) dim.ret=dim(R)[1]
+  RGr<- t(eigenG$vectors) %*% R %*% eigenG$vectors
+  df.s  <- data.frame(G=log(eigenG$value),
+                      R=log(diag(RGr)))
 
   # simmulations
   sim<-adply(1:sims, 1, function(i){
@@ -47,18 +52,14 @@ npEigentest<-function(G,means,phy,n.s,sims=1000,dim.ret=NULL,parallel=FALSE){
 
   quantiles <- sim %>% apply(.,2,function(c) stats::quantile(c, c(0.025,0.975)))
 
-
   # Quantiles for each test
   slt05<-obs$slt<quantiles[1,1] | obs$slt>quantiles[2,1] # if TRUE: reject drift
   SDrel05<-obs$SDrel<quantiles[1,2] | obs$SDrel>quantiles[2,2] # if TRUE: reject drift
 
-  return(list("TestResults" = data.frame(obs,
-                                  slt05,
-                                  SDrel05),
-              "Empirical" = obs,
-              "SimValues" = sim
-              ) )
-}
+  return(list("Empirical" = obs,
+              "SimValues" = sim,
+              "eigenvalues" = df.s))
+  }
 
 
 pcTests<-function(G,R,n,dim.ret=NULL){
@@ -68,7 +69,7 @@ pcTests<-function(G,R,n,dim.ret=NULL){
   df.s  <- data.frame(G=log(eigenG$value),
                       R=log(diag(RGr)))
   sltest<-lm(R~G, data=df.s[1:dim.ret,])
-  sltest<-sltest$coefficients[2]
+  sltest<-sltest$coefficients
 
   CRGr<-stats::cov2cor(RGr)[1:dim.ret,1:dim.ret]
   dimnames(CRGr)<-list(1:dim(CRGr)[1],1:dim(CRGr)[1])
@@ -82,6 +83,7 @@ pcTests<-function(G,R,n,dim.ret=NULL){
     N<-length(evs)
     SDrel<-sqrt(sum((mean(evs)-evs)^2))/sqrt(N-1)
   }
-  return(data.frame(slt=sltest[1],
+  return(data.frame(intercept=sltest[1],
+                    slope=sltest[2],
                     SDrel))
 }
