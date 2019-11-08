9c5d99ac0b6bea6a847adf71cfe1d936770e0d7b
VareciaRubra <annapontopenna@gmail.com>
2019-11-08 11:08:01 -0600

    itemized description sim_multiphylo

--- R/sim_multiphylo.R
+++ R/sim_multiphylo.R
@@ -29,6 +29,7 @@
 #' The 'sim_multiphylo' function returns an object of class "list". This is a
 #' list of items of class "matrix", all kxk.
 #' If 'matrix' = TRUE, the function returns 5 variance covariance matrices:
+#'   \itemize{
 #'   \item{R}{Evolutionary rate matrix. Describes the rate of evolution and
 #'   co-evolution among characters.
 #'   Diagonal contains traits' rate evolution according to Brownian-Motion,
@@ -37,10 +38,13 @@
 #'   \item{B}{Between matrix. Variance-vovariance between average species traits.}
 #'   \item{G}{Genetic matrix. The original aditive variance-covariance G matrix imputed.}
 #'   \item{A}{Selection matrix. The expected effect due to selection.}
+#'  }
 #' If 'matrix' = FALSE, the function returns 3 matrices:
+#'   \itemize{
 #'   \item{bdata}{Simulated trait values for species averages.}
 #'   \item{wdata}{Simulated intraspecific error. }
 #'   \item{G}{the original aditive variance-covariance G matrix imputed.}
+#'   }
 #' }
 #'
 
