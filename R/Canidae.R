b3117df0e7dd2e3f6432807ed8e7e3ce3d26c911
VareciaRubra <annapontopenna@gmail.com>
2019-11-08 16:10:54 -0600

    Canidae .R file

---
+++ R/Canidae.R
@@ -0,0 +1,9 @@
+#' @name Canidae
+#' @title Canidae
+#' @author Fabio Machado
+#' @docType data
+#' @description example dataset for mvEvol package
+#' @details example dataset for mvEvol package. contains four objects
+#' @keywords dataset
+#' @usage data(Canidae)
+'Canidae'
