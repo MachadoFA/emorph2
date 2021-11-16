5e338abeed162fd3356bb99b7859a3266300adf0
Fabio A. Machado <macfabio@gmail.com>
2021-11-16 16:24:07 -0500

    Update landeDist.R

--- R/landeDist.R
+++ R/landeDist.R
@@ -1,4 +1,4 @@
-#' Lande's generalized genetic distance
+#' Test for Lande's generalized genetic distance
 #'
 #' Test for the presence of preferencial lines of evolution among multiple
 #' independent lineages/replicates.
@@ -61,6 +61,18 @@ landeDist.test <-
   list(table=table, thresh=thresh)
 }
 
+
+#' Lande's generalized genetic distance
+#'
+#' Calculates Lande's generalized genetic distance
+#'
+#' @param dz
+#' @param G
+#' @param Ne
+#' @details
+#' @export
+#' @references Lande, R. 1979. Quantitative genetic analysis of multivariate evolution, applied to brain: body size allometry. Evolution, 33(1), 402–416.
+#' @author Fabio Andrade Machado
 
 landeDist<-function(dz, G, Ne){
   mahalanobis(dz, center=FALSE, cov=G) * Ne
